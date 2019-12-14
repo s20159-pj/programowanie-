c64babe8ea2cad6d3c63b2fd42d1ace10195cb8c
20159-pj <s20159@pjwstk.edu.pl>
2019-12-14 17:13:40 +0100

    drugie zadanie

---
+++ prg/program2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main ()
+{
+    std::string imie;
+    std::cout << "Jak masz na imie ?\n";
+    std::cin >> imie;
+    std::cout << "Elo " <<  imie << "!\n";
+    return 0;
+}
+
